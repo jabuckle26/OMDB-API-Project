3304eef554941ddab04d6cc943d237f347f6da6b
James Buckley <jabuckle@tcd.ie>
2019-10-23 10:03:40 +0100

    Added filter feature

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,6 +1,7 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=70b6f337
 
-let search = 'james'
+let search = 'james';
+let yearFilter = '';
 
 const buildList = (data) => {
     clearHTMLLIST();
@@ -26,48 +27,70 @@ const clearHTMLLIST = () => {
     })
 }
 
-const searchFilms = () =>  {
+const searchFilms = () => {
     //const userSearch = $("#searchBox").val().toString();
     const boxValue = document.getElementById('searchBox') as HTMLInputElement;
     const userSearch = boxValue.value;
-    boxValue.value =  '';
+    boxValue.value = '';
     search = userSearch;
-    getAPIData();
+    getAPIData(yearFilter);
 
 }
 
-const getAPIData = () => {
-fetch(`http://www.omdbapi.com/?s=${search}&apikey=70b6f337`)
-.then((response) => {
-    const newResponse = response.json();
-    return newResponse;
-})
-.then((data) => {return buildList(data)})
-.catch((error) => {
-    throw new Error(error)}
-);
+const getAPIData = (yearFilter) => {
+    if (yearFilter === '') {
+        fetch(`http://www.omdbapi.com/?s=${search}&apikey=70b6f337`)
+            .then((response) => {
+                const newResponse = response.json();
+                return newResponse;
+            })
+            .then((data) => { return buildList(data) })
+            .catch((error) => {
+                throw new Error(error)
+            }
+            );
+    } else {
+        console.log(yearFilter, 'HERE');
+        fetch(`http://www.omdbapi.com/?s=${search}&y=${yearFilter}&apikey=70b6f337`)
+        .then((response) => {
+            const newResponse = response.json();
+            return newResponse;
+        })
+        .then((data) => { return buildList(data) })
+        .catch((error) => {
+            throw new Error(error)
+        }
+        );
+    }
 }
 
 const checkEnterKey = (ev) => {
-    if (ev.code === 'Enter') {searchFilms()};
+    if (ev.code === 'Enter') { searchFilms() };
 }
 
 const showFilterBox = () => {
     let targetDiv = document.getElementById('filterWindow');
-    if (targetDiv.style.display === 'inline-block') {
+    if (targetDiv.style.display === 'flex') {
         //Switch to hidden
         targetDiv.style.display = "none";
-        document.getElementById('filterButton').style.color = "$activatedButton";
+        document.getElementById('filterButton').style.color = "white";
     } else {
         //Switch to visible
         targetDiv.style.display = "flex";
+        document.getElementById('filterButton').style.color = "$background-button1";
     }
-
 }
 
-document.getElementById('searchButton').addEventListener("click",searchFilms);
-document.getElementById('searchBox').addEventListener("keyup",checkEnterKey);
+const filterSearch = () => {
+    const boxValue = document.getElementById('filterYear') as HTMLInputElement;
+    yearFilter = boxValue.value;
+    console.log(yearFilter);
+    getAPIData(yearFilter);
+}
+
+document.getElementById('searchButton').addEventListener("click", searchFilms);
+document.getElementById('searchBox').addEventListener("keyup", checkEnterKey);
 document.getElementById('filterButton').addEventListener('click', showFilterBox);
+document.getElementById('selectFilter').addEventListener('click', filterSearch);
 
-
-getAPIData();
+getAPIData(yearFilter);
